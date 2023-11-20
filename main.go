d141500c9b7cf75c64e7d9ffdd5329316f46db95
tanaka <nitnc.tanaka.shikkobu@nezumi.be>
2023-11-20 09:22:34 +0900

    create

---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"bytes"
+	json2 "encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+const BaseURL = "http://127.0.0.1:6000"
+const User1 = "user1"
+const User2 = "user2"
+
+func main() {
+	client := &http.Client{}
+	logger := &log.Logger{}
+
+	prepareUser(client, User1, logger)
+	prepareUser(client, User2, logger)
+
+	unit := 10_000
+	count := 0
+	c := make(chan uint8, unit)
+
+	for i := 0; i < 100_000_000; i++ {
+		for j := 0; j < unit; j++ {
+			go func() {
+				for k := 0; k < 10; k++ {
+					if err := sendRandom(client, logger); err == nil {
+						c <- 1
+						return
+					}
+				}
+				panic("failed to send")
+			}()
+			for k := 0; k < unit; k++ {
+				<-c
+				count++
+			}
+			if err := measure(client, logger, count); err != nil {
+				logger.Printf("failed to measure")
+			}
+		}
+	}
+}
+
+func prepareUser(client *http.Client, username string, logger *log.Logger) {
+	url := fmt.Sprintf("%s/user/%s", BaseURL, username)
+	res, err := client.Post(url, "application/json", nil)
+	if err != nil {
+		logger.Fatalf("failed to prepare user: %s", err.Error())
+		return
+	}
+	if res.StatusCode != 200 {
+		logger.Fatalf("failed to prepare user: %d", res.StatusCode)
+		return
+	}
+}
+
+type SendJSON struct {
+	Point int `json:"point"`
+}
+
+func measure(client *http.Client, logger *log.Logger, count int) error {
+	start := time.Now()
+	url := fmt.Sprintf("%s/user/%s/log", BaseURL, User1)
+	res, err := client.Get(url)
+	if err != nil {
+		logger.Println(err.Error())
+		return err
+	}
+	defer res.Body.Close()
+	b, err := io.ReadAll(res.Body)
+	costDuration := time.Since(start)
+	logger.Println(fmt.Sprintf("measure: count: %d, status: %d, point: %s, time: %dms, %s", count, res.StatusCode, string(b), costDuration.Milliseconds(), time.Now().Format("2006-01-02T15:04:05+09:00")))
+	return nil
+}
+
+func sendRandom(client *http.Client, logger *log.Logger) error {
+	from, to := randomFromTo()
+	url := fmt.Sprintf("%s/send/%s/%s", BaseURL, from, to)
+	content := SendJSON{rand.Intn(1_000_000)}
+	marshalled, err := json2.Marshal(content)
+	if err != nil {
+		logger.Fatalf("failed to marshal: %s", err.Error())
+	}
+	req, err := http.NewRequest("POST", url, bytes.NewReader(marshalled))
+	if err != nil {
+		logger.Printf("failed to create request: %s", err.Error())
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		logger.Printf("failed to send request: %s", err.Error())
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		logger.Printf("failed to send request: %d", resp.StatusCode)
+		return fmt.Errorf("failed to send request")
+	}
+	return nil
+}
+
+func randomFromTo() (string, string) {
+	from := rand.Intn(2)
+	if from == 0 {
+		return User1, User2
+	}
+	return User2, User1
+}
