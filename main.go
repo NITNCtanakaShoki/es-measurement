ed9326ed10f2bb3ce4695a576e6456d30ada33b9
tanaka <nitnc.tanaka.shikkobu@nezumi.be>
2023-11-21 14:25:48 +0900

    メモリを変更

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ const User1 = "user1"
 const User2 = "user2"
 
 func main() {
-	title := "CHUNK_1024_APP_M100_C10_DB_M100_C50"
+	title := "CHUNK_1024_APP_M200_C50_DB_M100_C50"
 	logFile, err := os.OpenFile(fmt.Sprintf("%s.log", title), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
 		log.Fatal(err)
